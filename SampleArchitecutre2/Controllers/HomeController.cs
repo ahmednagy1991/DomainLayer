101110eb8bb5e97892ce1fbcc0620d85ba14d9ee
ahmed nagy <ahmed_nagy1991@hotmail.com>
2019-09-06 23:30:56 +0200

    comment some code

--- SampleArchitecutre2/Controllers/HomeController.cs
+++ SampleArchitecutre2/Controllers/HomeController.cs
@@ -20,16 +20,16 @@ namespace SampleArchitecutre2.Controllers
             ViewBag.Title = "Home Page";
             //var mode = _userService.Login("test", "testpass");
             //var val = mode.Validate();
-            var model = new UserModel();
-            model.Email = "test7";
-            model.Username = "test user7";
-            model.Password = "test password7";
-            model.Phone = "01023456";
-            var val = model.Validate();
-            if (val.IsValid)
-            {
-                _userService.Register(model);
-            }
+            //var model = new UserModel();
+            //model.Email = "test7";
+            //model.Username = "test user7";
+            //model.Password = "test password7";
+            //model.Phone = "01023456";
+            //var val = model.Validate();
+            //if (val.IsValid)
+            //{
+            //    _userService.Register(model);
+            //}
 
             return View();
         }
